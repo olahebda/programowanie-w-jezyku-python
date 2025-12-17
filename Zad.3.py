67c8a233ae096917791ecdf3ebd94591f4a44335
Aleksandra Grabacka <ola.hebdaaa@gmail.com>
2025-12-17 09:28:37 +0100

    Aktualizacja klas biblioteki

--- Zad.3.py
+++ Zad.3.py
@@ -1,1 +1,10 @@
-#python xyz
+class Property:
+    def __init__(self, area, rooms, price, address):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = address
+
+class Hosue(Property):
+    def __init__(selfgit):self, plot,)
+
