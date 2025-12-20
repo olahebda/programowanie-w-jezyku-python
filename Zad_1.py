57ec48850680d3d692145f73d9ae6452b6b21543
Aleksandra Grabacka <ola.hebdaaa@gmail.com>
2025-12-20 13:23:05 +0100

    Poprawa stylu kodu wg PEP8 w Zad_1.py i Zad_2.py

--- Zad_1.py
+++ Zad_1.py
@@ -1,15 +1,18 @@
+
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
+
     def is_passed(self):
-        if sum(self.marks) / len(self.marks)> 50:
+        if sum(self.marks) / len(self.marks) > 50:
             return True
         else:
             return False
 
-Student_1 = Student ("Jan", [30, 60, 80])
-Student_2 = Student ("Stanisław", [15, 20, 35])
+
+Student_1 = Student("Jan", [30, 60, 80])
+Student_2 = Student("Stanisław", [15, 20, 35])
 
 print(Student_1.is_passed())
 print(Student_2.is_passed())
--- Zad_2.py
+++ Zad_2.py
@@ -1,5 +1,9 @@
+
+
 class Library:
-    def __init__(self, city, street, zip_code, open_hours, phone):
+    def __init__(self, city, street,
+                 zip_code, open_hours,
+                 phone):
         self.city = city
         self.street = street
         self.zip_code = zip_code
@@ -7,10 +11,15 @@ class Library:
         self.phone = phone
 
     def __str__(self):
-        return f"Library address: {self.city}, {self.street}, {self.zip_code}, Phone: {self.phone}, Hours: {self.open_hours}"
+        return (f"Library address: {self.city}, {self.street}, "
+                f"{self.zip_code}, Phone: {self.phone},"
+                f" Hours: {self.open_hours}")
+
 
 class Employee:
-    def __init__(self, first_name, last_name, hire_date, birth_date, city, street, zip_code, phone):
+    def __init__(self, first_name, last_name,
+                 hire_date, birth_date, city,
+                 street, zip_code, phone):
         self.first_name = first_name
         self.last_name = last_name
         self.hire_date = hire_date
@@ -19,42 +28,99 @@ class Employee:
         self.street = street
         self.zip_code = zip_code
         self.phone = phone
+
     def __str__(self):
-        return f"Employee: {self.first_name} {self.last_name}, Hired: {self.hire_date}, Birth: {self.birth_date}, Address: {self.city}, {self.street}, {self.zip_code}, Phone_number: {self.phone}"
+        return (f"Employee: {self.first_name} {self.last_name},"
+                f" Hired: {self.hire_date}, Birth: {self.birth_date}, "
+                f"Address: {self.city}, {self.street}, "
+                f"{self.zip_code}, Phone_number: {self.phone}")
+
 
 class Student:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+
     def __str__(self):
-        return f"Name: {self.first_name}, Surname: {self.last_name}"
+        return (f"Name: {self.first_name}, "
+                f"Surname: {self.last_name}")
+
 
 class Book:
-    def __init__(self, library, publication_date, author_name, author_surname, number_of_pages):
+    def __init__(self, library, publication_date,
+                 author_name, author_surname,
+                 number_of_pages):
         self.library = library
         self.publication_date = publication_date
         self.author_name = author_name
         self.author_surname = author_surname
         self.number_of_pages = number_of_pages
+
     def __str__(self):
-        return f"Book from {self.library}, Published {self.publication_date} by: {self.author_name}, {self.author_surname}, Number of Pages: {self.number_of_pages} "
+        return (
+            f"Book from {
+                self.library}, " f"Published {
+                self.publication_date} by: {
+                self.author_name}," f" {
+                    self.author_surname}, Number of Pages: {
+                        self.number_of_pages} ")
+
 
 class Order:
-    def __init__(self, employee, student, books, order_date):
+    def __init__(self, employee, student,
+                 books, order_date):
         self.employee = employee
         self.student = student
-        self.books = books #lista
+        self.books = books  # lista
         self.order_date = order_date
+
     def __str__(self):
         books_str = ", ".join(str(book) for book in self.books)
-        return f"Employee: {self.employee}, Student: {self.student}, Books: {books_str}, Order Date: {self.order_date}"
+        return (f"Employee: {self.employee}, "
+                f"Student: {self.student}, "
+                f"Books: {books_str}, Order Date: {self.order_date}")
 
-Library1 = Library("Kraków", "Starowiślna", "32-612", "8:00-16:00", "600-500-800")
-Library2 = Library("Kraków", "Wincentego Witosa", "32-612", "8:00-14:00", "600-300-811")
 
-Employee1 = Employee("Jan", "Kowalski","10-03-2025","16-02-1992", "Kraków", "Zakopiańska", "33-990", "600-300-999")
-Employee2 = Employee("Jolanta", "Kropka","10-01-2023","16-02-1988", "Kraków", "Wielicka", "22-990", "600-375-999")
-Employee3 = Employee("Janina", "Szczotka","10-01-2018","16-02-1865", "Warszawa", "Długa", "22-888", "600-378-999")
+Library1 = Library(
+    "Kraków",
+    "Starowiślna",
+    "32-612",
+    "8:00-16:00",
+    "600-500-800")
+Library2 = Library(
+    "Kraków",
+    "Wincentego Witosa",
+    "32-612",
+    "8:00-14:00",
+    "600-300-811")
+
+Employee1 = Employee(
+    "Jan",
+    "Kowalski",
+    "10-03-2025",
+    "16-02-1992",
+    "Kraków",
+    "Zakopiańska",
+    "33-990",
+    "600-300-999")
+Employee2 = Employee(
+    "Jolanta",
+    "Kropka",
+    "10-01-2023",
+    "16-02-1988",
+    "Kraków",
+    "Wielicka",
+    "22-990",
+    "600-375-999")
+Employee3 = Employee(
+    "Janina",
+    "Szczotka",
+    "10-01-2018",
+    "16-02-1865",
+    "Warszawa",
+    "Długa",
+    "22-888",
+    "600-378-999")
 
 Student1 = Student("Jan", "Brzechwa")
 Student2 = Student("Teofil", "Jamróz")
@@ -66,9 +132,8 @@ Book3 = Book(Library2, "08-11-1976", "Remigiusz", "Mróz", 157)
 Book4 = Book(Library2, "24-11-1898", "Jan", "Kochanowski", 116)
 Book5 = Book(Library1, "02-07-1999", "Wisława", "Szymborska", 199)
 
-Order1 = Order(Employee1,Student1, [Book2,Book3, Book5], "14-02-1195")
-Order2 = Order(Employee2,Student3, [Book2, Book4, Book5], "10-03-2001")
+Order1 = Order(Employee1, Student1, [Book2, Book3, Book5], "14-02-1195")
+Order2 = Order(Employee2, Student3, [Book2, Book4, Book5], "10-03-2001")
 
 print(Order1)
 print(Order2)
-
